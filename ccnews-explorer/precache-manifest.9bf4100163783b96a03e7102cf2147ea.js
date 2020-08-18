9d9047c4443c7950c101282b48d3084961323ef9
Rodger Benham <rodger.benham@gmail.com>
2020-08-18 17:50:05 +1000

    Add ccnews explorer beta

---
+++ ccnews-explorer/precache-manifest.9bf4100163783b96a03e7102cf2147ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f9c405d762a6c4f4466b131ca7f0e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d787b379d0962bd84b8",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "217710ffc1bf7a7ba458",
+    "url": "/static/css/main.8f956753.chunk.css"
+  },
+  {
+    "revision": "3d787b379d0962bd84b8",
+    "url": "/static/js/2.4683ef52.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.4683ef52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217710ffc1bf7a7ba458",
+    "url": "/static/js/main.bd7e3211.chunk.js"
+  },
+  {
+    "revision": "d8e64fb9e35c609f6cbe",
+    "url": "/static/js/runtime-main.65a7d0f3.js"
+  }
+]);
